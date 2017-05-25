331fda83fa9b2f35d3fb8343fcb159bf8fd0e017
Miltos Nedelkos <nedelkosm@gmail.com>
2017-05-25 14:30:03 +0200

    Enforced delegate type in compile-time

--- EventHandler.cs
+++ EventHandler.cs
@@ -24,8 +24,7 @@ public class EventHandler {
     /// </summary>
     /// <typeparam name="T">The type of event to subscribe to. Must be a delegate</typeparam>
     /// <param name="listener">The listener that will be called when the event triggers</param>
-    public void Subscribe<T>(T listener) {
-        if (!typeof(MulticastDelegate).IsAssignableFrom(typeof(T))) { return; } // Check if generic is a delegate
+    public void Subscribe<T>(T listener) where T : MulticastDelegate {
         var key = typeof(T);
         if (!events.ContainsKey(key)) { events.Add(key, new List<System.Object>()); }
         if (!events[key].Contains(listener)) events[key].Add(listener);
@@ -36,8 +35,7 @@ public class EventHandler {
     /// </summary>
     /// <typeparam name="T">The type of event to subscribe to. Must be a delegate</typeparam>
     /// <param name="listener">The listener</param>
-    public void Unsubscribe<T>(T listener) {
-        if (!typeof(MulticastDelegate).IsAssignableFrom(typeof(T))) { return; } // Check if generic is a delegate
+    public void Unsubscribe<T>(T listener) where T : MulticastDelegate {
         var key = typeof(T);
         if (!events.ContainsKey(key)) { return; }
         events[key].Remove(listener);
@@ -49,8 +47,7 @@ public class EventHandler {
     /// </summary>
     /// <typeparam name="T">The type of event to be called. Must be a delegate</typeparam>
     /// <param name="parameters">Any parameters passed to this certain event. Must match the type of the delegate event.</param>
-    internal void Trigger<T>(params object[] parameters) {
-        if (!typeof(MulticastDelegate).IsAssignableFrom(typeof(T))) { return; } // Check if generic is a delegate
+    internal void Trigger<T>(params object[] parameters) where T : MulticastDelegate {
         if (!events.ContainsKey(typeof(T))) { // Check if event is registered
             System.Diagnostics.Debug.WriteLine("Event " + typeof(T) + " is not registered (no listeners were added yet).");
             // Exception is not thrown because listeners can still be added later.
